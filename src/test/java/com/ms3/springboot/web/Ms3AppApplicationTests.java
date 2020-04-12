e0f8fd8cee7a6c998b31f19141500b7c2e09c111
John James Libron <johnjames.libron@gmail.com>
2020-04-12 11:58:37 +0800

    Initial Commit

---
+++ src/test/java/com/ms3/springboot/web/Ms3AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms3.springboot.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ms3AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
