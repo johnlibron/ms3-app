5169fe23ec0975c711c982e8822a40857966a6fc
John James Libron <johnjames.libron@gmail.com>
2020-04-13 13:43:42 +0800

    ms3-app.log - logs for records (received, successful, failed)

--- src/main/java/com/ms3/app/controller/UploadController.java
+++ src/main/java/com/ms3/app/controller/UploadController.java
@@ -10,6 +10,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +19,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.ms3.app.Ms3AppApplication;
 import com.ms3.app.model.Record;
 import com.opencsv.CSVReader;
 import com.opencsv.CSVReaderBuilder;
@@ -25,6 +28,8 @@ import com.opencsv.ICSVWriter;
 
 @Controller
 public class UploadController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(Ms3AppApplication.class);
 
 	@GetMapping("/")
 	public String index(Model model) {
@@ -41,7 +46,7 @@ public class UploadController {
 		} else {
 			// parse CSV file to create a list of 'record' objects
 			try {
-			    // columns names
+				// columns names
 				String[] headerRecord = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J"};
 				
 				// create a reader
@@ -52,43 +57,57 @@ public class UploadController {
 				
 				// create a write
 				Writer writer = Files.newBufferedWriter(Paths.get("ms3Interview-bad.csv"));
-			    
-			    // create a CSV writer
-			    ICSVWriter csvWriter = new CSVWriterBuilder(writer).build();
-			    
-			    // write header record
-			    csvWriter.writeNext(headerRecord);
-			    
-			    List<Record> records = new ArrayList<>();
-			    
-			    Boolean checkHeader = null;
+				
+				// create a CSV writer
+				ICSVWriter csvWriter = new CSVWriterBuilder(writer).build();
+				
+				// write header record
+				csvWriter.writeNext(headerRecord);
+				
+				int recordsReceived = 0;
+				int recordsSuccessful = 0;
+				int recordsFailed = 0;
+				
+				List<Record> records = new ArrayList<>();
+				
+				Boolean checkHeader = null;
 
-			    // iterate through list of records
-			    for (String[] record : csvReader.readAll()) {
-			    	if (null == checkHeader) {
-			    		checkHeader = Arrays.equals(record, headerRecord);
-		    			if (!checkHeader) {
-		    				throw new Exception("Header doesn't match the column required.");
-		    			}
-			    	} else {
-				    	if (record.length == headerRecord.length) {
-				    		Record recordObj = new Record();
-					    	recordObj.setA(record[0]);
-					    	recordObj.setB(record[1]);
-					    	recordObj.setC(record[2]);
-					    	recordObj.setD(record[3]);
-					    	recordObj.setE(record[4]);
-					    	recordObj.setF(record[5]);
-					    	recordObj.setG(record[6]);
-					    	recordObj.setH(record[7]);
-					    	recordObj.setI(record[8]);
-					    	recordObj.setJ(record[9]);
-					    	records.add(recordObj);
-				    	} else {
-				    		csvWriter.writeNext(record);
-				    	}
-			    	}
-			    }
+				// iterate through list of records
+				for (String[] record : csvReader.readAll()) {
+					if (null == checkHeader) {
+						checkHeader = Arrays.equals(record, headerRecord);
+						if (!checkHeader) {
+							throw new Exception("Header doesn't match the column required.");
+						}
+					} else {
+						if (record.length == headerRecord.length) {
+							Record recordObj = new Record();
+							recordObj.setA(record[0]);
+							recordObj.setB(record[1]);
+							recordObj.setC(record[2]);
+							recordObj.setD(record[3]);
+							recordObj.setE(record[4]);
+							recordObj.setF(record[5]);
+							recordObj.setG(record[6]);
+							recordObj.setH(record[7]);
+							recordObj.setI(record[8]);
+							recordObj.setJ(record[9]);
+							records.add(recordObj);
+							recordsSuccessful++;
+						} else {
+							if (!isRecordEmpty(record)) {
+								csvWriter.writeNext(record);
+								recordsFailed++;
+							}
+						}
+					}
+				}
+				
+				recordsReceived = recordsSuccessful + recordsFailed;
+				
+				logger.info(recordsReceived + " of records received");
+				logger.info(recordsSuccessful + " of records successful");
+				logger.info(recordsFailed + " of records failed");
 
 				model.addAttribute("records", records);
 				model.addAttribute("status", true);
@@ -107,5 +126,16 @@ public class UploadController {
 		
 		return "upload";
 	}
-
+	
+	private boolean isRecordEmpty(String[] record) {
+		boolean isEmpty = true;
+		for (String str : record) {
+			if (str.length() > 0 || !str.isEmpty()) {
+				isEmpty = false;
+				break;
+			}
+		}
+		return isEmpty;
+	}
+	
 }
---
+++ src/test/java/com/ms3/app/Ms3AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms3.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ms3AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
